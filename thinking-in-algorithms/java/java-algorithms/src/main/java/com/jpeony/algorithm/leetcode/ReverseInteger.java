472e202814c9a7d47fe2ba26ee6ee0afe014ccfe
yihonglei <yihonglei@hqzhuanche.com>
2020-12-16 10:39:46 +0800

    Reverse interger

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/ReverseInteger.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/ReverseInteger.java
@@ -6,7 +6,28 @@ package com.jpeony.algorithm.leetcode;
  * @author yihonglei
  */
 public class ReverseInteger {
+
+    private static int reverse(int x) {
+        // 正负标识
+        int flag = 1;
+        if (x < 0) {
+            flag = -1;
+        }
+        // 取绝对值
+        x = x * flag;
+
+        // 反转
+        long res = 0;
+        while (x != 0) {
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (int)res != res ? 0 : (int)res * flag;
+    }
+
     public static void main(String[] args) {
-        
+        int y = -2147;
+        System.out.println(reverse(y));
     }
 }
