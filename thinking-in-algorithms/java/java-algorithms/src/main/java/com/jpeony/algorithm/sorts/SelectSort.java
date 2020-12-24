0613ee9c8fb525f24bd6877e1bf42a57681d0b1a
yihonglei <yihonglei@hqzhuanche.com>
2020-12-24 20:19:12 +0800

    insert sort

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/sorts/SelectSort.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/sorts/SelectSort.java
@@ -13,21 +13,38 @@ public class SelectSort {
             return;
         }
         // 总共要经过 N-1 轮比较
+//        for (int i = 0; i < n - 1; i++) {
+//            int minIndex = i;
+//            // 每轮需要比较的次数 N-i，已经比较出结果的，无需再比较
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[j] < arr[minIndex]) {
+//                    // 记录目前能找到的最小值元素的下标
+//                    minIndex = j;
+//                }
+//            }
+//
+//            // 将找到的最小值和i位置所在的值进行交换
+//            if (i != minIndex) {
+//                int tmp = arr[i];
+//                arr[i] = arr[minIndex];
+//                arr[minIndex] = tmp;
+//            }
+//        }
+
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-            // 每轮需要比较的次数 N-i，已经比较出结果的，无需再比较
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    // 记录目前能找到的最小值元素的下标
+            // 比较：取未排序区间最小值
+            int j = i;
+            for (; j < n; j++) {
+                if (arr[minIndex] > arr[j]) {
                     minIndex = j;
                 }
             }
-
-            // 将找到的最小值和i位置所在的值进行交换
+            // 交换：坐标相同，无需交换，否则，需要进行交换，将未排序区间最小值交换插入到已排序区间尾端
             if (i != minIndex) {
-                int tmp = arr[i];
-                arr[i] = arr[minIndex];
-                arr[minIndex] = tmp;
+                int tmp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = tmp;
             }
         }
     }
