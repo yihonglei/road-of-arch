3441f8fa4f3d82f3ccde76eeff175626ff812606
yihonglei <yihonglei@hqzhuanche.com>
2020-12-25 23:02:31 +0800

    valid anagram

---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/others/ValidAnagram.java
@@ -0,0 +1,32 @@
+package com.jpeony.algorithm.leetcode.others;
+
+import java.util.Arrays;
+
+/**
+ * @author yihonglei
+ */
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        for (int i = 0; i < sArr.length; i++) {
+            if (sArr[i] != tArr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("nanna", "nnnaa"));
+    }
+}
