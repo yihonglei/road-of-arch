35e88a0bf4019fc051715b85a4429b384a9fbb2a
yihonglei <yihonglei@hqzhuanche.com>
2021-01-01 23:10:45 +0800

    common

---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/others/PalindromeNumber.java
@@ -0,0 +1,36 @@
+package com.jpeony.algorithm.leetcode.others;
+
+/**
+ * 【题源】https://leetcode-cn.com/problems/palindrome-number/
+ * 回文数
+ *
+ * @author yihonglei
+ */
+public class PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        if (x < 10) {
+            return true;
+        }
+
+        if (x % 10 == 0) {
+            return false;
+        }
+
+        int y = 0;
+        int tmp = x;
+        while (tmp > 0) {
+            y = y * 10 + tmp % 10;
+            tmp = tmp / 10;
+        }
+
+        return y == x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
