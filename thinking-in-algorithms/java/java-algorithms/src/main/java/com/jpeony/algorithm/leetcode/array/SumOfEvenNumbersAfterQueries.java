7893a5204840f6af1102ef74143fbb3c2d5e9b7f
yihonglei <yihonglei@hqzhuanche.com>
2021-01-13 20:49:02 +0800

    SumOfEvenNumbersAfterQueries

---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/array/SumOfEvenNumbersAfterQueries.java
@@ -0,0 +1,23 @@
+package com.jpeony.algorithm.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 【题源】https://leetcode-cn.com/problems/sum-of-even-numbers-after-queries/
+ * 查询后的偶数和
+ *
+ * @author yihonglei
+ */
+public class SumOfEvenNumbersAfterQueries {
+    private static int[] sumEvenAfterQueries(int[] arr, int[][] queries) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int[][] queries = {{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
+
+        System.out.println(Arrays.toString(sumEvenAfterQueries(arr, queries)));
+    }
+}
