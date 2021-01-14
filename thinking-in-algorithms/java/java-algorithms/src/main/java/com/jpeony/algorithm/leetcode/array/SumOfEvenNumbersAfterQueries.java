5038224cd1da39ada5a1cf751ef9cae5d02ba782
yihonglei <yihonglei@hqzhuanche.com>
2021-01-14 21:31:45 +0800

    -

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/array/SumOfEvenNumbersAfterQueries.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/array/SumOfEvenNumbersAfterQueries.java
@@ -17,7 +17,6 @@ public class SumOfEvenNumbersAfterQueries {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4};
         int[][] queries = {{1, 0}, {-3, 1}, {-4, 0}, {2, 3}};
-
         System.out.println(Arrays.toString(sumEvenAfterQueries(arr, queries)));
     }
 }
