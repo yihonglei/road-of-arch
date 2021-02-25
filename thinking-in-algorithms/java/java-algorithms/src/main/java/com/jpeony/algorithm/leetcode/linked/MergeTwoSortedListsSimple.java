3e878cd24a5f9a5b118771e9b4e734adf102d6ea
yihonglei <yihonglei@hqzhuanche.com>
2021-02-25 09:55:15 +0800

    merge two lists

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/linked/MergeTwoSortedListsSimple.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/linked/MergeTwoSortedListsSimple.java
@@ -8,18 +8,18 @@ package com.jpeony.algorithm.leetcode.linked;
 public class MergeTwoSortedListsSimple {
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//        if (l1 == null) {
-//            return l2;
-//        } else if (l2 == null) {
-//            return l1;
-//        } else if (l1.val < l2.val) {
-//            l1.next = ;
-//        } else {
-//
-//        }
-        return null;
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1);
+            return l2;
+        }
     }
-
 
     /**
      * Definition for singly-linked list.
