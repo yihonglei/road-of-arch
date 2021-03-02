85556fc4af0431d2c0ef9fd16d91b31e8876d255
yihonglei <yihonglei@hqzhuanche.com>
2021-03-02 14:50:33 +0800

    deque

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/queue/DesignCircularDeque.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/queue/DesignCircularDeque.java
@@ -6,17 +6,37 @@ package com.jpeony.algorithm.leetcode.queue;
  * @author yihonglei
  */
 public class DesignCircularDeque {
+    private int capacity;
+    private int[] arr;
+    private int front;
+    private int rear;
+
     /**
      * Initialize your data structure here. Set the size of the deque to be k.
      */
     public DesignCircularDeque(int k) {
+        capacity = k + 1;
+        arr = new int[capacity];
 
+        // 头部指向第 1 个存放元素的位置
+        // 插入时，先减，再赋值
+        // 删除时，索引 +1（注意取模）
+        front = 0;
+        // 尾部指向下一个插入元素的位置
+        // 插入时，先赋值，再加
+        // 删除时，索引 -1（注意取模）
+        rear = 0;
     }
 
     /**
      * Adds an item at the front of Deque. Return true if the operation is successful.
      */
     public boolean insertFront(int value) {
+        if (isFull()) {
+            return false;
+        }
+        front = (front - 1 + capacity) % capacity;
+        arr[front] = value;
         return true;
     }
 
@@ -24,6 +44,11 @@ public class DesignCircularDeque {
      * Adds an item at the rear of Deque. Return true if the operation is successful.
      */
     public boolean insertLast(int value) {
+        if (isFull()) {
+            return false;
+        }
+        arr[rear] = value;
+        rear = (rear + 1) % capacity;
         return true;
     }
 
@@ -31,6 +56,11 @@ public class DesignCircularDeque {
      * Deletes an item from the front of Deque. Return true if the operation is successful.
      */
     public boolean deleteFront() {
+        if (isEmpty()) {
+            return false;
+        }
+        // front 被设计在数组的开头，所以是 +1
+        front = (front + 1) % capacity;
         return true;
     }
 
@@ -38,6 +68,11 @@ public class DesignCircularDeque {
      * Deletes an item from the rear of Deque. Return true if the operation is successful.
      */
     public boolean deleteLast() {
+        if (isEmpty()) {
+            return false;
+        }
+        // rear 被设计在数组的末尾，所以是 -1
+        rear = (rear - 1 + capacity) % capacity;
         return true;
     }
 
@@ -45,32 +80,41 @@ public class DesignCircularDeque {
      * Get the front item from the deque.
      */
     public int getFront() {
-        return -1;
+        if (isEmpty()) {
+            return -1;
+        }
+        return arr[front];
     }
 
     /**
      * Get the last item from the deque.
      */
     public int getRear() {
-        return -1;
+        if (isEmpty()) {
+            return -1;
+        }
+        // 当 rear 为 0 时防止数组越界
+        return arr[(rear - 1 + capacity) % capacity];
     }
 
     /**
      * Checks whether the circular deque is empty or not.
      */
     public boolean isEmpty() {
-        return true;
+        return front == rear;
     }
 
     /**
      * Checks whether the circular deque is full or not.
      */
     public boolean isFull() {
-        return true;
+        // 注意：这个设计是非常经典的做法
+        return (rear + 1) % capacity == front;
     }
 
     public static void main(String[] args) {
         DesignCircularDeque circularDeque = new DesignCircularDeque(3);
+
         circularDeque.insertLast(1);
         circularDeque.insertLast(2);
         circularDeque.insertFront(3);
