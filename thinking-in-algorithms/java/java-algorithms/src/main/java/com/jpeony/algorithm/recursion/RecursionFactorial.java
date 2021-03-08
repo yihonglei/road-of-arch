0ea3e69c0b7273f5c9663c42b224864440631c8d
yihonglei <yihonglei@hqzhuanche.com>
2021-03-08 16:23:03 +0800

    recursion

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/recursion/RecursionFactorial.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/recursion/RecursionFactorial.java
@@ -1,15 +1,13 @@
 package com.jpeony.algorithm.recursion;
 
 /**
- * 通过递归计算某个数的阶乘
+ * 递归计算阶乘
  *
  * @author yihonglei
  */
 public class RecursionFactorial {
-    /**
-     * 递归计算某个数的阶乘
-     */
-    public static int testFactorial(int n) {
+
+    private static int testFactorial(int n) {
         if (n == 1) {
             return 1;
         } else {
@@ -18,7 +16,6 @@ public class RecursionFactorial {
     }
 
     public static void main(String[] args) {
-        // 计算5的阶乘
         System.out.println(testFactorial(5));
     }
 }
--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/recursion/RecursionSequence.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/recursion/RecursionSequence.java
@@ -1,23 +1,17 @@
 package com.jpeony.algorithm.recursion;
 
 /**
- * 通过递归打印1~100
+ * 递归打印 1~100
  *
  * @author yihonglei
  */
 public class RecursionSequence {
     private static int i = 0;
 
-    /**
-     * 递归打印1到100
-     */
     private static void testRecursion01() {
-        // 自增
         i++;
-        // 小于等于100时回调，否则跳出回调，避免死循环
         if (i <= 100) {
             System.out.println(i);
-            // 回调: 自身调用自身
             testRecursion01();
         } else {
             System.out.println("Game Over!");
@@ -25,7 +19,6 @@ public class RecursionSequence {
     }
 
     public static void main(String[] args) {
-        // 打印1~100
         testRecursion01();
     }
 }
