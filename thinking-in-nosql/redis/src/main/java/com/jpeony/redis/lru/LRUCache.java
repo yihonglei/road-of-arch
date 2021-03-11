3a07939c6a9b92fef89f4a6d3327511a546f12bc
yihonglei <yihonglei@hqzhuanche.com>
2021-03-11 11:58:02 +0800

    lru cache

---
+++ thinking-in-nosql/redis/src/main/java/com/jpeony/redis/lru/LRUCache.java
@@ -0,0 +1,30 @@
+package com.jpeony.redis.lru;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author yihonglei
+ */
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private final int CACHE_SIZE;
+
+    /**
+     * 传递进来最多能缓存多少数据
+     *
+     * @param cacheSize 缓存大小
+     */
+    public LRUCache(int cacheSize) {
+        super((int) Math.ceil(cacheSize / 0.75) + 1, 0.75f, true);
+        CACHE_SIZE = cacheSize;
+    }
+
+    /**
+     * 钩子方法，通过 put 新增键值对的时候，若该方法返回 true 便移除该 map 中最老的键和值
+     */
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        // 当 map中的数据量大于指定的缓存个数的时候，就自动删除最老的数据。
+        return size() > CACHE_SIZE;
+    }
+}
