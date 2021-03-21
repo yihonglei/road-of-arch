02b920ab6fbc7ea3f667e0ff4906a2e45451015e
yihonglei <yihonglei@hqzhuanche.com>
2021-03-21 10:21:19 +0800

    init

---
+++ thinking-in-zookeeper/src/main/java/com.jpeony.zookeeper/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.jpeony.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot启动类
+ *
+ * @author yihonglei
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
