8deba0c63d46ac3971b5a04ef2af1996b0628669
yihonglei <yihonglei@hqzhuanche.com>
2021-03-29 19:42:19 +0800

    registry

--- thinking-in-dubbo/dubbo-provider-xml/src/main/java/com/jpeony/dubbo/simple/SimpleClient.java
+++ thinking-in-dubbo/dubbo-provider-xml/src/main/java/com/jpeony/dubbo/simple/SimpleClient.java
@@ -2,6 +2,7 @@ package com.jpeony.dubbo.simple;
 
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import com.jpeony.dubbo.service.ProviderService;
 
 /**
@@ -9,7 +10,7 @@ import com.jpeony.dubbo.service.ProviderService;
  */
 public class SimpleClient {
 
-    public ProviderService buildRemoteService(String remoteUrl) {
+    public ProviderService buildRemoteServiceOrigin(String remoteUrl) {
         ReferenceConfig<ProviderService> referenceConfig = new ReferenceConfig<>();
         referenceConfig.setInterface(ProviderService.class);
         referenceConfig.setUrl(remoteUrl);
@@ -18,9 +19,21 @@ public class SimpleClient {
         return referenceConfig.get();
     }
 
+    public ProviderService buildRemoteServiceRegistry() {
+        ReferenceConfig<ProviderService> referenceConfig = new ReferenceConfig<>();
+        referenceConfig.setInterface(ProviderService.class);
+        referenceConfig.setApplication(new ApplicationConfig("client-app"));
+        referenceConfig.setRegistry(new RegistryConfig("zookeeper://127.0.0.1:2181"));
+
+        return referenceConfig.get();
+    }
+
     public static void main(String[] args) {
         SimpleClient client = new SimpleClient();
-        ProviderService providerService = client.buildRemoteService("dubbo://127.0.0.1:20880/com.jpeony.dubbo.service.ProviderService");
+        // 原始调用
+//        ProviderService providerService = client.buildRemoteServiceOrigin("dubbo://127.0.0.1:20880/com.jpeony.dubbo.service.ProviderService");
+        // 注册中心
+        ProviderService providerService = client.buildRemoteServiceRegistry();
         System.out.println(providerService.sayHello("hello"));
     }
 }
