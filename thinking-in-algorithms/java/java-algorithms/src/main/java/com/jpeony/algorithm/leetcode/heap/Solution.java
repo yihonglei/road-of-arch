7f4fde01875fced83140b8852f18a67669b12925
yihonglei <yihonglei@hqzhuanche.com>
2021-06-15 19:56:56 +0800

    heap

---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/heap/Solution.java
@@ -0,0 +1,13 @@
+package com.jpeony.algorithm.leetcode.heap;
+
+/**
+ * 【寻找第 K 大】
+ * https://www.nowcoder.com/practice/e016ad9b7f0b45048c58a9f27ba618bf?tpId=188&&tqId=38572&rp=1&ru=/activity/oj&qru=/ta/job-code-high-week/question-ranking
+ *
+ * @author yihonglei
+ */
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+}
--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/linked/LinkedListCycleLCCI.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/linked/LinkedListCycleLCCI.java
@@ -1,7 +1,6 @@
 package com.jpeony.algorithm.leetcode.linked;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
