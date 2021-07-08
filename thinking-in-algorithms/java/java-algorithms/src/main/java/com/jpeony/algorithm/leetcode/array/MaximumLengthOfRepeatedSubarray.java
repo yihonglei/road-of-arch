c940d1fc7c972321b9ae409adbba23912ffe5969
yihonglei <yihonglei@hqzhuanche.com>
2021-07-08 20:05:22 +0800

    lru

---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/array/MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,22 @@
+package com.jpeony.algorithm.leetcode.array;
+
+/**
+ * 【题源】https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray/
+ *
+ * @author yihonglei
+ */
+public class MaximumLengthOfRepeatedSubarray {
+
+    public static int findLength(int[] nums1, int[] nums2) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 2, 1};
+        int[] nums2 = {3, 2, 1, 4, 7};
+
+        int len = findLength(nums1, nums2);
+        System.out.println("len = " + len);
+    }
+}
---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/linked/LRUCache.java
@@ -0,0 +1,8 @@
+package com.jpeony.algorithm.leetcode.linked;
+
+/**
+ * @author yihonglei
+ */
+public class LRUCache {
+
+}
