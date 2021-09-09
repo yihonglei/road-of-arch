83476133deb6b1bf1e81eaa243159fb313d43061
yihonglei <jpeony@163.com>
2021-09-09 14:26:39 +0800

    sorting the sentence

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/sorts/SortingTheSentence.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/sorts/SortingTheSentence.java
@@ -1,6 +1,5 @@
 package com.jpeony.algorithm.leetcode.sorts;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -9,18 +8,34 @@ import java.util.HashMap;
  * @author yihonglei
  */
 public class SortingTheSentence {
+    /**
+     * 散列表
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     */
     public static String sortSentence(String s) {
-
-        return "";
-    }
-
-    public static void main(String[] args) {
-        String s = "is2 sentence4 This1 a3";
         String[] sArr = s.split(" ");
 
         HashMap<String, String> maps = new HashMap<>();
         for (String str : sArr) {
-
+            int len = str.length();
+            String key = str.substring(len - 1);
+            String value = str.substring(0, len - 1);
+            maps.put(key, value);
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= sArr.length; i++) {
+            String keyIndex = String.valueOf(i);
+            sb.append(maps.get(keyIndex));
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String s = "is2 sentence4 This1 a3";
+        System.out.println(sortSentence(s));
     }
 }
