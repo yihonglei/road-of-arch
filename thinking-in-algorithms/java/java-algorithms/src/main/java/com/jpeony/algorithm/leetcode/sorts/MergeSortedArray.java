240acfc3dd0b14f76c30a045aab08365d8251b8d
yihonglei <jpeony@163.com>
2021-09-24 02:15:50 +0800

    merge sorted array by two points

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/sorts/MergeSortedArray.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/sorts/MergeSortedArray.java
@@ -9,10 +9,11 @@ import java.util.Arrays;
  */
 public class MergeSortedArray {
     /**
+     * 【直接排序】将两个数组合并，然后用插入排序进行排序
      * 时间复杂度：O(m*n^2)
      * 空间复杂度：O(m+n)
      */
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void mergeOne(int[] nums1, int m, int[] nums2, int n) {
         // 合并
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
@@ -36,12 +37,44 @@ public class MergeSortedArray {
         }
     }
 
+    /**
+     * 【双指针】两个有序数组，每次找出最小的值进行入队
+     * 时间复杂度：O(m+n)
+     * 空间复杂度：O(m+n)
+     */
+    public static void mergeTwo(int[] nums1, int m, int[] nums2, int n) {
+        // p1 为 nums1 头指针，p2 为 nums2 头指针
+        int p1 = 0, p2 = 0;
+        // 排序数组
+        int[] sorted = new int[m + n];
+        // 合并数组
+        int cur; // 当前元素
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur  = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
+            }
+            sorted[p1 + p2 -1] = cur;
+        }
+
+        for (int i = 0; i < (m + n); i ++) {
+            nums1[i] = sorted[i];
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int m = 3;
         int[] nums2 = {2, 5, 6};
         int n = 3;
-        merge(nums1, m, nums2, n);
+//        mergeOne(nums1, m, nums2, n);
+
+        mergeTwo(nums1, m, nums2, n);
 
         System.out.println(Arrays.toString(nums1));
     }
