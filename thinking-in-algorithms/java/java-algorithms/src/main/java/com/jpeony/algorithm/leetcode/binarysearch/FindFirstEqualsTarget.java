3e56d222e864bf5b544f81c18005be72a53ad977
yihonglei <jpeony@163.com>
2021-10-23 17:52:13 +0800

    binarysearch: find first equals target

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/binarysearch/FindFirstEqualsTarget.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/binarysearch/FindFirstEqualsTarget.java
@@ -7,4 +7,33 @@ package com.jpeony.algorithm.leetcode.binarysearch;
  */
 public class FindFirstEqualsTarget {
 
+    private static int search(int[] nums, int target) {
+        // 低位
+        int low = 0;
+        // 高位
+        int high = nums.length - 1;
+        while (low <= high) {
+            // 中位
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] < target) {
+                    return mid;
+                }
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int targetIndex = search(nums, target);
+        System.out.println("targetIndex = " + targetIndex);
+    }
 }
