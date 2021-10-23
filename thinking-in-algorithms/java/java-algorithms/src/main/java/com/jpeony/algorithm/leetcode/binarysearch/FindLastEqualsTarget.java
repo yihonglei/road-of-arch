5783253c31c849cb5a99939342395e0516ea675c
yihonglei <jpeony@163.com>
2021-10-23 18:10:07 +0800

    binarysearch: find last equals target

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/binarysearch/FindLastEqualsTarget.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/binarysearch/FindLastEqualsTarget.java
@@ -6,5 +6,32 @@ package com.jpeony.algorithm.leetcode.binarysearch;
  * @author yihonglei
  */
 public class FindLastEqualsTarget {
+    private static int search(int[] nums, int target) {
+        // 低位
+        int low = 0;
+        // 高位
+        int high = nums.length - 1;
+        while (low <= high) {
+            // 中位
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                if (mid == nums.length - 1 || nums[mid + 1] > target) {
+                    return mid;
+                }
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int targetIndex = search(nums, target);
+        System.out.println("targetIndex = " + targetIndex);
+    }
 }
