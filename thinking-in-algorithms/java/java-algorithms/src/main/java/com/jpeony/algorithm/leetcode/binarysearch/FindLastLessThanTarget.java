f24ca030bdc89fe3ecd48ab217c5cfe7807e210b
yihonglei <jpeony@163.com>
2021-10-23 18:46:09 +0800

    binarysearch: find last less than target

--- thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/binarysearch/FindLastLessThanTarget.java
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/binarysearch/FindLastLessThanTarget.java
@@ -6,5 +6,31 @@ package com.jpeony.algorithm.leetcode.binarysearch;
  * @author yihonglei
  */
 public class FindLastLessThanTarget {
+    private static int search(int[] nums, int target) {
+        // 低位
+        int low = 0;
+        // 高位
+        int high = nums.length - 1;
+        while (low <= high) {
+            // 中位
+            int mid = low + (high - low) / 2;
+            if (nums[mid] <= target) {
+                if (mid == nums.length - 1 || nums[mid + 1] > target) {
+                    return mid;
+                }
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
 
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 9;
+        int targetIndex = search(nums, target);
+        System.out.println("targetIndex = " + targetIndex);
+    }
 }
