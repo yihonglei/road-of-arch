37ca43f557360cb31db4842d4b0bc95c2d6cab58
yihonglei <jpeony@163.com>
2021-12-04 21:02:33 +0800

    common

---
+++ thinking-in-algorithms/java/java-algorithms/src/main/java/com/jpeony/algorithm/leetcode/string/LongestPalindromicSubstring.java
@@ -0,0 +1,18 @@
+package com.jpeony.algorithm.leetcode.string;
+
+/**
+ * 【题源】https://leetcode-cn.com/problems/longest-palindromic-substring/
+ *
+ * @author yihonglei
+ */
+public class LongestPalindromicSubstring {
+    private static String longestPalindrome(String s) {
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String s = "babad";
+        String ps = longestPalindrome(s);
+        System.out.println("Longest palindromic substring is: " + ps);
+    }
+}
