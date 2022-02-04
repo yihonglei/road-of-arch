916f29e3857f1e62e5a4e2719fdb70cf2e1355cd
yihonglei <jpeony@163.com>
2022-02-04 17:27:11 +0800

    netty

--- thinking-in-netty/src/main/java/com/jpeony/netty/nio/NIOServer.java
+++ thinking-in-netty/src/main/java/com/jpeony/netty/nio/NIOServer.java
@@ -15,15 +15,21 @@ public class NIOServer {
     private static Selector selector;
 
     public static void main(String[] args) throws IOException {
+        // 1、打开 ServerSocketChannel
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         serverSocketChannel.configureBlocking(false);
 
+        // 2、绑定监听地址 InetSocketAddress
         ServerSocket serverSocket = serverSocketChannel.socket();
         serverSocket.bind(new InetSocketAddress(8989));
 
+        // 3、创建 Selector，启动线程
         selector = Selector.open();
+
+        // 4、将 ServerSocketChannel 注册到 Selector，监听 SelectionKey.OP_ACCEPT
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
+        // 5、轮询就绪的 key
         while (true) {
             // 当注册事件到达时，方法返回，否则该方法会一直阻塞
             selector.select();
@@ -32,9 +38,11 @@ public class NIOServer {
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 iterator.remove();
+                // 6、handleAccept 处理新的客户端接入
                 if (key.isAcceptable()) {
                     handleAccept(key);
                 } else if (key.isReadable()) {
+                    // 8、handleRead 异步读请求
                     handleRead(key);
                 }
             }
@@ -49,6 +57,7 @@ public class NIOServer {
         String msg = "Hello Client";
         socketChannel.write(ByteBuffer.wrap(msg.getBytes()));
 
+        // 7、向 Selector 注册监听读操作
         socketChannel.register(selector, SelectionKey.OP_READ);
     }
 
