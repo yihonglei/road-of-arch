e9929f0a755ee293216d2e570fa97615c84e08e0
yihonglei <jpeony@163.com>
2023-05-20 09:34:22 +0800

    c

---
+++ thinking-in-c/src/cmake-build-debug/CMakeFiles/Thinking-in-C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Thinking-in-C.dir/main.c.o"
+  "Thinking-in-C"
+  "Thinking-in-C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Thinking-in-C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ thinking-in-c/src/main.c
@@ -0,0 +1,9 @@
+//
+// Created by hongqi on 2023/5/20.
+//
+
+#include "stdio.h"
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
