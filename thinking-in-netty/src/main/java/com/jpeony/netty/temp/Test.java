361539cdff1ad548a618598219cd638e51e33261
yihonglei <jpeony@163.com>
2024-02-17 23:25:34 +0800

    common

---
+++ thinking-in-netty/src/main/java/com/jpeony/netty/temp/Test.java
@@ -0,0 +1,8 @@
+package com.jpeony.netty.temp;
+
+public class Test {
+    public static void main(String[] args) {
+        String s = "abcde";
+        System.out.println(s.substring(1, 3));
+    }
+}
