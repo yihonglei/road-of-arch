1d22b616a88de5300dd6c3c670c3f31ce6f09e5f
yihonglei <jpeony@163.com>
2024-03-05 16:41:48 +0800

    common

--- thinking-in-c/src/main.c
+++ thinking-in-c/src/main.c
@@ -6,14 +6,26 @@
 
 #include "hello.h"
 
-#define NUM1 10
+#define NUM1 10 // 定义宏常量
+
+typedef struct { // 结构
+    int ip;
+    int port;
+} CONN;
 
 int main() {
-    printf("NUM1: %d", NUM1);
-
+    // 宏常量
+    printf("NUM1: %d\n", NUM1);
+    // 多文件函数调用
     int nums[] = {1, 2, 2};
-
     int left = removeDuplicates(nums, 3);
-    printf("left: %d", left);
+    printf("left: %d\n", left);
+    // typedef
+    const CONN conns[] = {{1, 2},
+                          {3, 4}};
+    for (int i = 0; i < (sizeof(conns) / sizeof(CONN)); i++) {
+        printf("ip:%d, port:%d\n", conns[i].ip, conns[i].port);
+    }
+
     return 0;
 }
