258f1a73ebf6818d0e5c371f7b88e86324a0d856
yihonglei <jpeony@163.com>
2024-03-15 16:53:49 +0800

    netty

--- thinking-in-netty/src/main/java/com/jpeony/netty/bio/SocketServer.java
+++ thinking-in-netty/src/main/java/com/jpeony/netty/bio/SocketServer.java
@@ -8,7 +8,8 @@ import java.net.Socket;
  * 1、bio 单线程时接受客户端请求慢；
  * 2、bio 多线程时并发量高会创建大量线程，系统线程开启数量有限，系统资源不够崩掉，
  * 你可以开线程池处理，但是当访问量剧增时，处理不了成千上万的并发量；
- * 因为阻塞 IO 会由于频繁的 wait 导致 IO 线程经常性的阻塞，线程无法进行高效的工作，IO 处理能力自然下降。
+ * 因为阻塞 IO 会由于频繁的 wait 导致 IO 线程经常性的阻塞，线程无法进行高效的工作，
+ * IO 处理能力自然下降，大量请求依然阻塞。
  * 3、bio 支撑不了高并发访问；
  *
  * @author yihonglei
@@ -38,7 +39,6 @@ public class SocketServer {
         public void run() {
             try {
                 byte[] input = new byte[1024];
-
                 // Read request from the client socket
                 socket.getInputStream().read(input);
                 // process
