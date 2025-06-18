4efdfe701ed8bdba9feca50e3afbfa82ee7e7d0e
yihonglei <jpeony@163.com>
2025-06-18 22:32:48 +0800

    common

--- thinking-in-c/src/main.c
+++ thinking-in-c/src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main () {
-    printf("Hello，world! \n");
+#define N 5
+int main(void) {
+    int arr[] = {1, 2, 3, 4, 5};
+    int* p = (int*) malloc(sizeof(int) * N);
+    memcpy(p, arr, sizeof(int) * N);
+    for (int i = 0; i < N; ++i) {
+       printf("%d\n", *(p + i));
+    }
+
+    free(p);
     return 0;
 }
